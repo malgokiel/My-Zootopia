f1d41ea63c66a23ae996e608f82b74f45bfd4ae4
malgorzata <libronika@gmail.com>
2025-01-15 10:39:18 +0100

    improve code structure

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -11,7 +11,7 @@ def extract_animal_information(animals_data):
     The Function extracts information about each animal into a string:
     - name - diet - top location - type
     """
-    output = ""
+    formatted_animal_info = ""
     for animal in animals_data:
         animal_name = animal.get("name", None)
         animal_diet = animal["characteristics"].get("diet", None)
@@ -19,15 +19,26 @@ def extract_animal_information(animals_data):
         animal_type = animal["characteristics"].get("type", None)
         animal_selected_info = {"Diet": animal_diet, "Location": animal_location, "Type": animal_type}
 
-        output += '<li class="card__item">'
-        output += f'<div class="card__title">{animal_name}</div>'
-        output += '<p class="card__text">'
-        for information_type, information_value in animal_selected_info.items():
-            if information_value is not None:
-                output += f"<strong>{information_type}:</strong> {information_value}<br/>\n"
-        output += '</p>'
-        output += '</li>'
-    return output
+        formatted_animal_info += serialize_animal(animal_name, animal_selected_info)
+
+    return formatted_animal_info
+
+
+def serialize_animal(animal_name, animal_selected_info):
+    """
+    Formats a string with animal information to ensure desired html formatting
+    """
+    serialized_animal = ""
+    serialized_animal += '<li class="card__item">'
+    serialized_animal += f'<div class="card__title">{animal_name}</div>'
+    serialized_animal += '<p class="card__text">'
+    for information_type, information_value in animal_selected_info.items():
+        if information_value is not None:
+            serialized_animal += f"<strong>{information_type}:</strong> {information_value}<br/>\n"
+    serialized_animal += '</p>'
+    serialized_animal += '</li>'
+
+    return serialized_animal
 
 
 def add_animal_data_to_html(animals_data):
