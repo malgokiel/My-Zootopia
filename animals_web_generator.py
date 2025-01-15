5a77ec808b0bef4dc5016faa3136e5cd585013d6
malgorzata <libronika@gmail.com>
2025-01-15 15:44:55 +0100

    hardcoded html subbed with Flask

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,4 +1,9 @@
 import json
+from flask import Flask, render_template
+
+# App configuration
+app = Flask(__name__)
+
 
 def load_data(file_path):
     """ Loads a JSON file """
@@ -11,53 +16,31 @@ def extract_animal_information(animals_data):
     The Function extracts information about each animal into a string:
     - name - diet - top location - type
     """
-    formatted_animal_info = ""
+    all_animals_info = []
     for animal in animals_data:
         animal_name = animal.get("name", None)
         animal_diet = animal["characteristics"].get("diet", None)
         animal_location = animal["locations"][0]
         animal_type = animal["characteristics"].get("type", None)
-        animal_selected_info = {"Diet": animal_diet, "Location": animal_location, "Type": animal_type}
+        animal_selected_info = {"name": animal_name,
+                                "diet": animal_diet,
+                                "location": animal_location,
+                                "type": animal_type}
 
-        formatted_animal_info += serialize_animal(animal_name, animal_selected_info)
+        all_animals_info.append(animal_selected_info)
+    return all_animals_info
 
-    return formatted_animal_info
 
-
-def serialize_animal(animal_name, animal_selected_info):
+@app.route("/", methods=["GET"])
+def animals_template():
     """
-    Formats a string with animal information to ensure desired html formatting
+    Passes a list of animal dictionaries
+    and renders a template
     """
-    serialized_animal = ""
-    serialized_animal += '<li class="card__item">'
-    serialized_animal += f'<div class="card__title">{animal_name}</div>'
-    serialized_animal += '<p class="card__text">'
-    for information_type, information_value in animal_selected_info.items():
-        if information_value is not None:
-            serialized_animal += f"<strong>{information_type}:</strong> {information_value}<br/>\n"
-    serialized_animal += '</p>'
-    serialized_animal += '</li>'
-
-    return serialized_animal
-
-
-def add_animal_data_to_html(animals_data):
-    """
-    Opens html file and passes animal information as age content
-    """
-    with open("animals_template.html", "r") as fileobj:
-        template = fileobj.read()
-
-    new_phrase = template.replace("__REPLACE_ANIMALS_INFO__", extract_animal_information(animals_data))
-
-    with open("animals_template.html", "w") as fileobject:
-        fileobject.write(new_phrase)
-
-
-def main():
     all_animals_data = load_data('animals_data.json')
-    add_animal_data_to_html(all_animals_data)
+    animals = extract_animal_information(all_animals_data)
+    return render_template("animals_template.html", animals=animals)
 
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
